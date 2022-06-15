1aedbb5585c6e74e59fb495e4e5d51e11c8fe852
Zen <muhzaini30@gmail.com>
2022-06-16 06:01:08 +0800

    refactor: package.json for cjs

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import {load} from "js-yaml"
-import toSource from "tosource"
+import { load } from "js-yaml";
+import toSource from "tosource";
 
-const fileRegex = /\.ya?ml$/
+const fileRegex = /\.ya?ml$/;
 
-export default function VitePluginYaml(){
-    return {
-        name: 'yaml-to-js',
+export default function VitePluginYaml() {
+  return {
+    name: "yaml-to-js",
 
-        transform(src, id) {
-            if (fileRegex.test(id)) {
-                // src
+    transform(src, id) {
+      if (fileRegex.test(id)) {
+        // src
 
-                const transformedCode = `const data = ${toSource(load(src))}\n`;
-                const result = transformedCode + 'export default data';
+        const transformedCode = `const data = ${toSource(load(src))}\n`;
+        const result = transformedCode + "export default data";
 
-                return {
-                    code: result,
-                    map: null // provide source map if available
-                }
-            }
-        }
-    }
+        return {
+          code: result,
+          map: null, // provide source map if available
+        };
+      }
+    },
+  };
 }
