1caee9b176d0701f9ee9dd2c8dc891c934b6ab1c
Zen <muhzaini30@gmail.com>
2023-01-30 21:12:44 +0800

    coba tambah tipe

--- yaml.d.ts
+++ yaml.d.ts
@@ -1,8 +1,8 @@
 declare module "*.yaml" {
-  const value: Record<string, any>;
+  const value: Record<string, any> | ArrayLike<unknown>;
   export default value;
 }
 declare module "*.yml" {
-  const value: Record<string, any>;
+  const value: Record<string, any> | ArrayLike<unknown>;
   export default value;
 }
